9da567a0d9967d99dc9869cf9edf55d5fecd7879
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2022-11-05 20:09:21 -0700

    added list itteration and css jinja ext

--- flask_app/controllers/nasa.py
+++ flask_app/controllers/nasa.py
@@ -9,20 +9,20 @@ week_ago = today - DT.timedelta(days=7)
 print (week_ago)
 print (today)
 
-@app.route('/')
-def hello_world():
+@app.route('/nasa')
+def hello_nasa():
     api_key=creds.api_key
     data = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&start_date={week_ago}&end_date={today}'
     response = requests.get(data)
     
     nasa_data = response.json()
+    nasa_data =list(reversed(nasa_data))
     # print(nasa_data)
     # print(nasa_data[1])
     picture = nasa_data[1]["url"]
     picture2 = nasa_data[2]["url"]
     picture3 = nasa_data[3]["url"]
-    print(nasa_data[1]["url"])
-    print(nasa_data[2]["url"])
-    print(nasa_data[3]["url"])
+    for i in range(len(nasa_data)):
+        print(nasa_data[i]["url"])
 
-    return render_template('index.html', picture=picture, picture2=picture2, picture3 =picture3)
+    return render_template('index.html', picture=picture, picture2=picture2, picture3 =picture3, nasa_data=nasa_data)
